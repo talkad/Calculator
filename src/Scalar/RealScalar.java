ef05decb85f93063d83d7d7e0df998ad6049b4bd
tal kadosh <talkad@post.bgu.ac.il>
2019-04-16 16:22:38 +0300

    finished

--- src/Scalar/RealScalar.java
+++ src/Scalar/RealScalar.java
@@ -55,10 +55,6 @@ public class RealScalar implements Scalar{
 	} 
 	
 	public String toString() {
-//		String pos=String.valueOf(num);
-//		String temp=pos.substring(pos.indexOf('.'));
-//		if(temp.length()!=-1 && temp.length()>4 && pos.length()>4)
-//			return pos.substring(0, pos.indexOf('.'))+ pos.substring(pos.indexOf('.'), pos.indexOf('.')+4);
 		DecimalFormat numberFormat = new DecimalFormat("#.###");
 		String output=numberFormat.format(num);
 		if(output.equals("-1"))
--- src/Scalar/Scalar.java
+++ src/Scalar/Scalar.java
@@ -4,15 +4,10 @@ public interface Scalar {
 		
 	public Scalar add(Scalar s);
 	public Scalar mul(Scalar s);
-	public Scalar mul(int num); //what?
+	public Scalar mul(int num); 
 	public Scalar pow(int exponent);
 	public Scalar neg();	
 	public boolean equals(Scalar s);
 	public String toString();
 	
-	/*
-	 * You can extend this interface / abstract class with reasonable methods
-that you find necessary to complete the task (Think about derivate of a
-poly term – what is missing?).
-	 */
 }
